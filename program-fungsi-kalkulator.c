aed7083bc3f5c4646b1473cb96a948b97ec235e4
abdyarysada <114853713+abdyarysada@users.noreply.github.com>
2022-10-19 09:24:19 +0800

    Update program-fungsi-kalkulator.c

--- program-fungsi-kalkulator.c
+++ program-fungsi-kalkulator.c
@@ -19,12 +19,23 @@ int main(){
     printf("\n||  Program Menghitung Kecepatan dan Menghitung   ||");
     printf("\n||           Luas dan Keliling Persegi            ||");
     printf("\n----------------------------------------------------");
-    printf("\nPilih Bangun Ruang: ");
+    printf("\nPilih rumus yang digunakan: ");
     printf("\n 1. Kecepatan");
     printf("\n 2. Luas dan Keliling Persegi");
     printf("\n----------------------------------------------------");
     printf("\nInputkan pilihan: ");
-    scanf("%d", &menu);
+    while (scanf("%d", &menu) == 0 || menu < 1 || menu > 2 )
+    {
+        printf("--------------------------------------------------------------------------");
+        printf("\nKarakter yang Anda inputkan salah!");
+        printf("\nSilahkan memilih angka 1 atau 2 untuk memilih rumus  ");
+        printf("\n------------------------------------------------------------------------");
+        printf("\nMasukkan angka untuk memilih rumus yang akan digunakan : ");
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    system("clear");
+
 
     if(menu==1){
     printf("\n---------------------------------------------------");
@@ -36,8 +47,6 @@ int main(){
     float waktu;
     printf("\nMasukan nilai waktu : ");
     scanf("%f",&waktu);
-
-
     printf("\n-----------------------------------------------");
     printf("\nKecepatannya adalah %.2f m/s", hitungKecepatan(jarak,waktu));
     printf("\n\nProgram Telah Dijalankan!\n\n\n");
